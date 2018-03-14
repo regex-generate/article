4a3957e97146cf6e545bfca4b21fbbc175523131
Drup <drupyog@zoho.com>
2018-03-14 23:27:43 +0100

    Muh shell script.

---
+++ measure/haskell_csv.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+set -f #Disable globing
+
+
+ReBase=('a*' '(ab*)*' '~(a*)b')
+ReMore=('a*' 'a*b' '(ab*)*' '~(a*)b' '((a|b)(a|b))*' '(1(01*0)*1|0)*')
+
+BackendBase=("naive" "ref")
+BackendMore=("seg")
+
+function genH {
+    file="$2_$1_haskell.csv"
+    echo "Regex $2 on backend $1 to $file"
+    re-generate-exe \
+        --alphabet "ab" -s20 \
+        -b "${1^}Star" \
+        "$2" > "$file"
+}
+
+function go {
+    local -n Bs=$1
+    local -n ReS=$2
+    echo "(${ReS[@]}) with (${Bs[@]})"
+    for B in "${Bs[@]}"
+    do
+        for Re in "${ReS[@]}"
+        do
+            genH $B $Re
+        done
+    done
+}
+    
+go BackendBase ReBase
+
+echo "Gnuploting to haskell_all.png!"
+gnuplot haskell_all.gnuplot
+
+go BackendMore ReMore
+
+echo "Gnuploting to haskell_langs.png!"
+gnuplot haskell_langs.gnuplot
